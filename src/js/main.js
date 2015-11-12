2e74a9201bfe6e4a38c5aa19c0519b050f17edaf
nullice <ui@nullice.com>
2015-11-12 08:59:51 +0800

    action bar

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,66 @@
 
 
 
+var Fontages = function ()
+{
+    this.length = 0;
+
+    this.font =
+    {
+        name: "",
+        family: "",
+        postScriptName: "",
+        style: "",
+
+        firstname: "",
+        tags_lang: [],       //语言
+        tags_com: [],        //发行商
+        tags_type: [],       //类型
+        tags_weight: [],     //字重
+        tags_user: [],       //用户自定义
+        tags_other: [],      //其他
+
+        _type: "Font"
+    }
 
 
+    this.list = [];
+
+    var fontGroup =
+    {
+        font: {},
+        _type: "FontGroup"
+    }
 
 
+}
 
 
+Fontages.prototype.add = function (name, family, postScriptName, style)
+{
 
-var Fontages {}
+    var font =
+    {
+        name: name,
+        family: family,
+        postScriptName: postScriptName,
+        style: style,
 
+        firstname: "",
+        tags_lang: [],       //语言
+        tags_com: [],        //发行商
+        tags_type: [],       //类型
+        tags_weight: [],     //字重
+        tags_user: [],       //用户自定义
+        tags_other: [],      //其他
+
+        _type: "Font"
+    }
+
+
+    this.list[this.list.length]=1;
+
+}
 
 
 
